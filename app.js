5426975b133fe2f25c6ebfd2fdaaf0d696cc9e36
colinnguyen95 <colinnguyen@Colins-iMac.local>
2019-12-09 23:40:40 -0800

    added documentation and login/signup functionality

--- app.js
+++ app.js
@@ -3,6 +3,33 @@
 //     accessToken: '_-8gfgQbjRke6f59m_O5m1Jo4BvADnwMRcP3P6_8D-Q'
 // })
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//Fetching the course content from courses.json file
 class Courses {
   async getCourses(){
     try{
@@ -23,48 +50,6 @@ class Courses {
   }
 }
 
-// class Topics {
-//   async getTopics(){
-//     try{
-//       let result = await fetch("../topics.json");
-//       let data = await result.json();
-//       let topics = data.items;
-
-//       topics = topics.map(item => {
-//         const { title, description } = item.fields;
-//         const { id } = item.sys;
-//         const image = item.fields.image.fields.file.url;
-//         return { title, description, id, image }
-//       })
-//       return topics;
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-// }
-
-// class Lectures {
-//   async getLectures(){
-//     try{
-//       let result = await fetch("lectures.json");
-//       let data = await result.json();
-//       let courses = data.items;
-
-//       courses = courses.map(item => {
-//         const { title, description } = item.fields;
-//         const { id } = item.sys;
-//         const image = item.fields.image.fields.file.url;
-//         return { title, description, id, image }
-//       })
-//       return courses;
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-// }
-
-
-
 class UI  {
   //display all courses
   displayCourses(courses){
@@ -82,57 +67,14 @@ class UI  {
       `;
     });
     document.querySelector(".wrapper").innerHTML = result;
-    // window.location.href = "./course-pages/JS.html"
   }
-
-  // //display all topics
-  // displayTopics(topics){
-  //   console.log(topics);
-  //   let result = '';
-  //   topics.forEach(topic => {
-  //     result +=`
-  //     <div class="Card">
-  //         <h3>${topic.title}</h3>
-  //         <p>${topic.description}</p>
-  //         <img src=${topic.image}>
-  //     </div>
-  //     `;
-  //   });
-  //   document.querySelector(".topics").innerHTML = result;
-  // }
-
-  // //display all lectures
-  // displayLectures(lectures){
-  //   console.log(lectures);
-  //   let result = '';
-  //   lectures.forEach(lecture => {
-  //     result +=`
-  //     <a href="./course-pages/${lecture.title.replace(/\s+/g, '-').toLowerCase()}.html">
-  //     <div class="Card">
-  //         <h3>${lecture.title}</h3>
-  //         <p>${lecture.description}</p>
-  //         <img src=${lecture.image}>
-  //     </div>
-  //     </a>
-  //     `;
-  //   });
-  //   document.querySelector(".lectures").innerHTML = result;
-  // }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const courses = new Courses();
-  // const topics = new Topics();
-  // const lectures = new Lectures();
 
   //get all courses
   courses.getCourses().then(courses => ui.displayCourses(courses));
-
-  // //get all topics
-  // topics.getTopics().then(topics => ui.displayTopics(topics));
-
-  //get all lectures
-  // lectures.getLectures().then(lectures => ui.displayLectures(lectures));
   console.log("hello")
 })
